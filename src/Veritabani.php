eaf5d1b8b8c39924bf0065616ef7a14587f942b3
osemk <onurerginer@gmail.com>
2023-12-09 22:54:00 +0300

    changes according to packagist

--- src/Veritabani.php
+++ src/Veritabani.php
@@ -7,6 +7,8 @@
  * This file is subject to the MIT license that is bundled
  * with this source code in the LICENSE.md file.
  */
+ 
+	namespace CustomModel;
 class Veritabani{
 var $baglan;
 var $sorgu;
